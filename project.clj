7520ee094de46db417d4bc60a90195172af9ed00
Paula Gearon <pgearon@cisco.com>
2019-07-24 18:27:57 -0400

    Initial release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject qtest "0.1.0-SNAPSHOT"
+(defproject org.clojars.quoll/qtest "0.1.0"
   :description "Testing utility functions"
   :url "https://github.com/quoll/qtest"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
